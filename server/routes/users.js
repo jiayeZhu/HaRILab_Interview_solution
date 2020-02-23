06e15c8262774b5200468dd4a9cd5d677bc17d8a
JiayeZhu <1352953@tongji.edu.cn>
2020-02-23 01:24:05 -0500

    finish user's api, add authCheck middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const UserRouter = express.Router()
-const {check, validationResult} = require('express-validator')
+const {check, query} = require('express-validator')
 const {UserModel, AttackModel} = require("../models")
-const {MakeResponse} = require('../utils')
+const {MakeResponse, checkValidationResult, checkAuth} = require('../utils')
+const _ = require('lodash')
 
-UserRouter.get('/', function(req, res, next) {
-  res.json({errorCode:0, msg:'router for user'})
-})
 
 /**
  * 
@@ -49,12 +47,10 @@ UserRouter.post('/',[
   //username should be string and not empty
   check('username').notEmpty().isString(),
   //uuid should be string and not empty
-  check('uuid').notEmpty().isString()
-], async (req, res, next) => {
+  check('uuid').notEmpty().isString(),
   //check validation result
-  const validationErrors = validationResult(req)
-  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
-
+  checkValidationResult
+], async (req, res, next) => {
   //check username and uuid existance
   let {username, uuid} = req.body
   try {
@@ -70,6 +66,8 @@ UserRouter.post('/',[
   let User = new UserModel({username,uuid})
   try {
     let {_id} = await User.save()
+    req.session.level = 'user'
+    req.session.userId = _id
     return MakeResponse(res,0,{userId:_id})
   } catch (error) {
     console.error("create user failed with error: ",error.message)
@@ -77,6 +75,64 @@ UserRouter.post('/',[
   }
 })
 
+
+/**
+ * 
+ * @api {POST} /api/user/:userId/session Create new session
+ * @apiName CreatSession
+ * @apiGroup User
+ * @apiVersion  0.0.1
+ * 
+ * @apiParam  (Path Parameter) {MongoId}  userId    user's unique userId
+ * @apiParam  (Body Parameter) {String}   username  user's username.
+ * @apiParam  (Body Parameter) {String}   uuid      uuid of user's device.
+ * 
+ * @apiSuccess (Succeeded) {Number} errorCode   response error code, should be 0.
+ * @apiSuccess (Succeeded) {String} msg         message to client, should be ''.
+ * 
+ * @apiError   (Failed) {HTTP-StatusCode} statusCode  http status code.
+ * 
+ * @apiParamExample  {json} Request-Example:
+ * {
+ *     username : "JohnDoe123",
+ *     uuid : "84AE7AA1-7000-4696-8A74-4FD588A4A5C7"
+ * }
+ * 
+ * @apiSuccessExample {json} Success-Response-Example:
+ * {
+ *     errorCode : 0,
+ *     msg : ''
+ * }
+ * 
+ * @apiErrorExample {HTTP-StatusCode} Error-Response-Example:
+ * 401 Unauthorized
+ * 
+ */
+UserRouter.post('/:userId/session',[
+  //userId should be mongoId
+  check('userId').notEmpty().isMongoId(),
+  //username should be string
+  check('username').notEmpty().isString(),
+  //uuid should be string
+  check('uuid').notEmpty().isString(),
+  //check validation result
+  checkValidationResult
+], async(req, res, next) =>{
+  let {userId} = req.params
+  let {username,uuid} = req.body
+  //check user identification
+  try {
+    let result = await UserModel.findById(userId).exec()
+    if (result === null || result.username !== username || result.uuid !== uuid) return res.sendStatus(401)
+  } catch (error) {
+    console.error('user authorization failed with error message: ',error.message)
+    return MakeResponse(res,999)
+  }
+  //set the session identifiers for future auth check
+  req.session.level = 'user'
+  req.session.userId = userId
+  return MakeResponse(res,0)
+})
 
 /**
  * 
@@ -116,17 +172,17 @@ UserRouter.post('/',[
  * 
  */
 UserRouter.post('/:userId/attack',[
+  //check auth
+  checkAuth,
   //date should be ISO8601 date
   check('date').notEmpty().isISO8601(),
   //location should be in [0,1] 0 means outside, 1 means inside
   check('location').notEmpty().isIn([0,1]),
   //userid should be valid mongoid
-  check('userId').notEmpty().isMongoId()
-],async (req,res,nect)=>{
+  check('userId').notEmpty().isMongoId(),
   //check validation result
-  const validationErrors = validationResult(req)
-  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
-  
+  checkValidationResult
+],async (req,res,nect)=>{
   //check user existance
   let {date,location} = req.body
   let {userId} = req.params
@@ -200,6 +256,8 @@ UserRouter.post('/:userId/attack',[
  * 
  */
 UserRouter.put('/:userId/attack/:attackId',[
+  //check auth
+  checkAuth,
   //date should be ISO8601 date
   check('date').notEmpty().isISO8601(),
   //location should be in [0,1] 0 means outside, 1 means inside
@@ -207,12 +265,10 @@ UserRouter.put('/:userId/attack/:attackId',[
   //userid should be valid mongoid
   check('userId').notEmpty().isMongoId(),
   //attackId should be valid mongoid
-  check('attackId').notEmpty().isMongoId()
-],async (req,res,next)=>{
+  check('attackId').notEmpty().isMongoId(),
   //check validation result
-  const validationErrors = validationResult(req)
-  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
-
+  checkValidationResult
+],async (req,res,next)=>{
   //check user and attack existance
   let {date, location} = req.body
   let {userId, attackId} = req.params
@@ -266,15 +322,15 @@ UserRouter.put('/:userId/attack/:attackId',[
  * 
  */
 UserRouter.delete('/:userId/attack/:attackId',[
+  //check auth
+  checkAuth,
   //userid should be valid mongoid
   check('userId').notEmpty().isMongoId(),
   //attackId should be valid mongoid
-  check('attackId').notEmpty().isMongoId()
-], async (req,res,nest)=>{
+  check('attackId').notEmpty().isMongoId(),
   //check validation result
-  const validationErrors = validationResult(req)
-  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
-
+  checkValidationResult
+], async (req,res,nest)=>{
   //check user and attack existance
   let {userId, attackId} = req.params
   try {
@@ -302,12 +358,119 @@ UserRouter.delete('/:userId/attack/:attackId',[
   }
 })
 
-UserRouter.get('/:userId/attack',[],async (req,res,next)=>{
+/**
+ * 
+ * @api {GET} /api/user/:userId/attack Search one's attack records
+ * @apiName SearchUserAttack
+ * @apiGroup User
+ * @apiVersion  0.0.1
+ * 
+ * @apiParam  (Path Parameter)  {String}                      userId    unique userId.
+ * @apiParam  (Query Parameter) {String}                      [from]    search attacks since this date (included).
+ * @apiParam  (Query Parameter) {String}                      [to]      search attacks until this date (included).
+ * @apiParam  (Query Parameter) {String="users","attacks"}    [source]  search collection source. default is "attacks" for a complete result but may be slower. "users" collection should be faster(not tested yet) but only remain records since 7 days before today.
+ * 
+ * @apiSuccess (Succeeded) {Number}     errorCode     response error code, should be 0.
+ * @apiSuccess (Succeeded) {Array}      msg           array of result attacks.
+ * @apiSuccess (Succeeded) {MongoId}    msg._id       objectId of the attack record.
+ * @apiSuccess (Succeeded) {Date}       msg.date      the date that attack happened in ISO8601 format.
+ * @apiSuccess (Succeeded) {Number=0,1} msg.location  the location when the attack happened. 0 means outside, 1 means inside
+ * 
+ * @apiError   (Failed) {Number} errorCode   response error code, should not be 0.
+ * @apiError   (Failed) {String} msg         error message
+ * 
+ * @apiSuccessExample {json} Success-Response-Example:
+ * {
+ *     errorCode : 0,
+ *     msg : [
+ *        {
+ *          _id: "5e50b83b51c16451e0f56322",
+ *          date: "2020-02-21T23:55:28.264Z",
+ *          location: 1
+ *        }
+ *      ]
+ * }
+ * 
+ * @apiErrorExample {json} Error-Response-Example:
+ * {
+ *   errorCode : 301,
+ *   msg : "invalid search source"
+ * }
+ * 
+ */
+UserRouter.get('/:userId/attack',[
+  //check auth first
+  checkAuth,
+  //userId should be valid mongoid
+  check('userId').notEmpty().isMongoId(),
+  //from should be valid date but not necessary
+  query('from').optional().isISO8601(),
+  //to shoud be valid date but noe necessary
+  query('to').optional().isISO8601(),
   //check validation result
-  const validationErrors = validationResult(req)
-  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
-  
-
+  checkValidationResult
+],async (req,res,next)=>{
+  let {userId} = req.params
+  //construct search engine option object
+  let searchEngineOption = {}
+  //append options to the option obejct if exist
+  if(req.query.from) searchEngineOption.from = req.query.from
+  if(req.query.to) searchEngineOption.to = req.query.to
+  if(req.query.source) searchEngineOption.source = req.query.source
+  //don't need to check user existance since the query result will be null if userId doesn't exist
+  try {
+    let result = await AttackSearchEngine(userId,searchEngineOption)
+    return MakeResponse(res,0,result)
+  } catch (error) {
+    if(error.message.includes('invalid source:')) return MakeResponse(res,301)
+    console.error("query attack failed with error: ",error.message)
+    return MakeResponse(res,999)
+  }
 })
 
 module.exports = UserRouter
+
+/**
+ * 
+ * @param {MongoId} userId  objectId of the user
+ * @param {Object}  option  search engine configuration
+ * @param {Date}    [option.from] search record since this date (included)
+ * @param {Date}    [option.to]   search record until this date (included)
+ * @param {String}  [option.source] search collection source, "users" or "attacks"
+ */
+function AttackSearchEngine(userId,option){
+  //check search source
+  //default search source will be "attacks" collection
+  //throw Error if source is not in ["attacks","users"]
+  let source = (option.source) ? (["attacks","users"].includes(option.source) ? option.source : false) : "attacks"
+  if(!source) throw Error("invalid source:"+option.source)
+  
+  //construct search condition object
+  let searchCondition = {userId}
+  //construct date limitation object
+  let dateLimitation = {}
+  
+  //check if there is any limitation on date
+  if(option.from) dateLimitation.$gte = option.from
+  if(option.to) dateLimitation.$lte = option.to
+  
+  //if there is limitation, append the limitation to the search condition object
+  if(!_.isEmpty(dateLimitation))  searchCondition.date=dateLimitation
+
+  //return the rsult Promise
+  if(source === 'attacks'){
+    return AttackModel.find(searchCondition)
+                      .select({_id:1, date:1, location:1})
+                      .exec()
+  }else{
+    return UserModel.findById(userId)
+                    .select('attacks')
+                    .populate({
+                      path: 'attacks',
+                      select: {_id:1, date:1, location:1},
+                      match: {date: searchCondition.date ? searchCondition.date : {}}
+                    })
+                    .exec()
+                    .then(r=>Promise.resolve(r.attacks)) //post process to make sure the outputs follow the same format
+  }
+}
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,3 +1,5 @@
+const {validationResult} = require('express-validator')
+
 const codeMsgMapping = {
   0:'',
   101: 'input validation failed',
@@ -5,6 +7,7 @@ const codeMsgMapping = {
   103: 'userId does not exist',
   201: 'userId/attackId does not exist or not match',
   202: 'the attack at that time was already reported',
+  301: 'invalid search source',
   999: 'Internal error occured'
 }
 /**
@@ -18,4 +21,30 @@ function MakeResponse(res,errorCode=0,data=null){
   return res.json({errorCode,msg})
 }
 
-module.exports = {MakeResponse}
+// middleware for checking validation result
+function checkValidationResult (req,res,next){
+  const validationErrors = validationResult(req)
+  if(!validationErrors.isEmpty()) return MakeResponse(res,101)
+  next()
+}
+// middleware for checking auth level.
+// user could only CRUD on his/her own data.
+// clinician could CRUD on all data.
+function checkAuth(req,res,next){
+  //check if it's a authed request
+  if(!req.session.level) return res.sendStatus(401)
+  //if it's user, check the userId in session and the userId in path parameter, they should match
+  if(req.session.level === 'user'){
+    if (req.session.userId !== req.params.userId) return res.sendStatus(403)
+    else next()
+    return
+  } 
+  //if it's clinician, pass
+  if(req.session.level === 'clinician') next()
+  else {
+    console.error('unknown auth level: ',req.session.level,' ,failed in checkAuth')
+    return MakeResponse(res,999)
+  }
+}
+
+module.exports = {MakeResponse,checkValidationResult,checkAuth}
